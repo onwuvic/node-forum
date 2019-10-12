17ae40b1cf20a6577f9a5c9295641bf6900a61a3
Onwuzor Victor <victor.onwuzor@andela.com>
2019-10-12 01:36:34 +0100

    add user seeders

--- src/database/migrations/20191011235851-create-user.js
+++ src/database/migrations/20191011235851-create-user.js
@@ -1,33 +1,42 @@
-'use strict';
+/* eslint-disable no-unused-vars */
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      firstName: {
-        type: Sequelize.STRING
-      },
-      lastName: {
-        type: Sequelize.STRING
-      },
-      email: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Users', {
+    id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      unique: true,
+      primaryKey: true,
+    },
+    fullName: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    email: {
+      type: Sequelize.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        isEmail: true
       }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Users');
-  }
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    gender: {
+      type: Sequelize.ENUM,
+      values: ['male', 'female']
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+  }),
+
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('Users')
 };
--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,11 +1,34 @@
-'use strict';
-module.exports = (sequelize, DataTypes) => {
+export default (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      unique: true,
+      primaryKey: true,
+    },
+    fullName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    gender: {
+      type: DataTypes.ENUM,
+      values: ['male', 'female'],
+      allowNull: false,
+    }
   }, {});
-  User.associate = function(models) {
+
+  // eslint-disable-next-line no-unused-vars
+  User.associate = (models) => {
     // associations can be defined here
   };
   return User;
