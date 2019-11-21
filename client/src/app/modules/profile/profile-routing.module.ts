5ba7b90de9cf93d122cdb2ac34eecd1ba6ad6cf8
Onwuzor Victor <victor.onwuzor@andela.com>
2019-11-21 18:43:14 +0100

    add profile frontend

---
+++ client/src/app/modules/profile/profile-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ProfileComponent } from './pages/profile/profile.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: ':username',
+        component: ProfileComponent
+      }
+    ]
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ProfileRoutingModule { }
