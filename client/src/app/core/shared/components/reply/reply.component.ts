0817a7b70f27825aae2e75f072379a7991660325
Onwuzor Victor <onwuzorvictor@outlook.com>
2020-12-09 21:31:10 +0100

    remove emitting favorite to replies

--- client/src/app/core/shared/components/reply/reply.component.ts
+++ client/src/app/core/shared/components/reply/reply.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material';
+
+import { ReplyService } from '../../../../core/services/reply/reply.service';
 
 @Component({
   selector: 'app-reply',
@@ -11,13 +14,16 @@ export class ReplyComponent implements OnInit, OnChanges {
   @Input() reply: any;
   @Input() isLogin: boolean;
   @Input() userId: number;
-  @Output() clickFavorite = new EventEmitter();
   @Output() clickDeleteReply = new EventEmitter();
   @Output() clickUpdateReply = new EventEmitter();
   editing = false;
   isFavorite: boolean;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(
+    private fb: FormBuilder,
+    private replyService: ReplyService,
+    private snackBar: MatSnackBar
+  ) { }
 
   ngOnInit() {
     this.editReplyForm = this.fb.group({
@@ -35,10 +41,6 @@ export class ReplyComponent implements OnInit, OnChanges {
     }
   }
 
-  onClickFavorite() {
-    this.clickFavorite.emit(this.isFavorite);
-  }
-
   editReply(body) {
     this.editing = true;
     this.editReplyForm.patchValue({
@@ -52,6 +54,43 @@ export class ReplyComponent implements OnInit, OnChanges {
 
   onUpdateReply() {
     this.clickUpdateReply.emit(this.editReplyForm.value);
+  }
+
+  toggle() {
+    this.isFavorite ? this.unfavoriteReply() : this.favoriteReply();
+  }
+
+  favoriteReply() {
+    this.replyService.addFavorite(this.reply.id)
+      .subscribe(
+        (data) => {
+          this.reply.favorites.push(data);
+          this.isFavorite = this.checkIsFavorite();
+        },
+        (error) => {
+          this.snackBar.open(error, 'Ok', {
+            panelClass: ['error']
+          });
+        }
+      );
+  }
+
+  unfavoriteReply() {
+    this.replyService.unFavorite(this.reply.id)
+      .subscribe(
+        (data) => {
+          const foundIndex = this.reply.favorites.findIndex(favorite => favorite.id === data.favoriteId);
+          if (foundIndex > -1) {
+            this.reply.favorites.splice(foundIndex, 1);
+          }
+          this.isFavorite = this.checkIsFavorite();
+        },
+        (error) => {
+          this.snackBar.open(error, 'Ok', {
+            panelClass: ['error']
+          });
+        }
+      );
   }
 
   /*
